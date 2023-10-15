9f128fbe6fc91b00dced3e3467afabe06f6166a9
ShawnZhuPro <114783966+ShawnZhuPro@users.noreply.github.com>
2023-10-14 21:24:34 -0700

    Create Sequential Command Draft

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -60,6 +60,15 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return ccw;
+    return new SequentialCommandGroup(
+      new AutoDrive(dt, 1.0),
+      new PIDTurnCCW(-90, dt),
+      new AutoDrive(dt, 1.0)
+
+      // This runs both autodrive and turn commands at the same time
+      new ParallelCommandGroup(
+          new AutoDrive(dt, 1.0), new PIDTurnCCW(90, dt)
+      )
+    );
   }
 }
